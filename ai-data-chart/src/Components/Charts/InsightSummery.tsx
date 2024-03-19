1e39a54a60e79b7ced022f912bd7293229a63527
vaibhav <vaibhavzade802@gmail.com>
2024-03-19 22:36:59 +0530

    completed

--- ai-data-chart/src/Components/Charts/InsightSummery.tsx
+++ ai-data-chart/src/Components/Charts/InsightSummery.tsx
@@ -28,12 +28,12 @@ export default function InsightSummery({  }: Props) {
               label: 'no of queries',
               data: [insight_summary.total_queries,insight_summary.successful_queries,insight_summary.failed_queries,insight_summary.average_response_time],
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132 )',
+                'rgba(54, 162, 235 )',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192 )',
+                'rgba(153, 102, 255)',
+               
               ],
               borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -41,7 +41,7 @@ export default function InsightSummery({  }: Props) {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+            
               ],
               borderWidth: 1
             }]
